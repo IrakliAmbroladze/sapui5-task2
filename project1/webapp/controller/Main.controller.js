458d2cfc6718cb822b9d17abb744d48dc583de64
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-19 19:36:39 +0400

    feat: define JSONMOdel in Main.controller

--- project1/webapp/controller/Main.controller.js
+++ project1/webapp/controller/Main.controller.js
@@ -1,10 +1,11 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("project1.controller.Main", {
-        onInit() {
-        }
+      onInit() {},
     });
-});
+  },
+);
+
